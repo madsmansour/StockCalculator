b5a1cc070d2d718c0f11014cfbeaf2c452bcf7ec
Martin Pedersen <42993919+MartinLieb@users.noreply.github.com>
2019-02-21 12:38:58 +0100

    Initial commit

---
+++ app/src/main/java/com/example/stockcalculator/Calculator.java
@@ -0,0 +1,12 @@
+package com.example.stockcalculator;
+
+public class Calculator {
+
+
+    public static double Breakeven(double a, double b, double c){
+        double resultat = (a * b + c) / a;
+
+        return resultat;
+
+    }
+}
---
+++ app/src/main/java/com/example/stockcalculator/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.stockcalculator;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText a;
+    private EditText b;
+    private EditText c;
+    private EditText resultat;
+    private Button button;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        a = findViewById(R.id.aTextView);
+        b = findViewById(R.id.bTextView);
+        c = findViewById(R.id.cTextView);
+        resultat = findViewById(R.id.resultatTextView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // your handler code here
+                // TODO træk a og b ud fra edittext
+
+                String aStringVal  = (a.getText().toString());
+                double aDoubleVal = Double.parseDouble(aStringVal);
+                String bStringVal  = (b.getText().toString());
+                double bDoubleVal = Double.parseDouble(bStringVal);
+                String cStringVal  = (c.getText().toString());
+                double cDoubleVal = Double.parseDouble(cStringVal);
+
+                // TODO  læg dem sammen
+                double resultatValue = Calculator.Breakeven(aDoubleVal,bDoubleVal,cDoubleVal);
+                Log.d("hajmund", "Resultatet er " + resultatValue);
+
+                // TODO  vis det i resultat
+                resultat.setText(String.valueOf(resultatValue));
+
+            }
+        });
+    }
+
+
+
+
+}
